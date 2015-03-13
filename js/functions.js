c525b16371a32a4eb038bd74cf9bdd8b27af5cde
dariusblajan <dariusblajan@gmail.com>
2015-03-13 21:48:22 +0200

    added 'personal' widget- all widgets collapsable

--- js/functions.js
+++ js/functions.js
@@ -19,6 +19,36 @@ newsWidget.getElementsByClassName("header")[0].onclick = function () {
     }
 };
 
+// collapse/expand help widget
+var helpWidget = document.getElementById("help-widget");
+helpWidget.getElementsByClassName("header")[0].onclick = function () {
+    if (helpWidget.className == "widget") {
+        helpWidget.className = "widget collapsed";
+    } else {
+        helpWidget.className = "widget";
+    }
+};
+
+//collapse/expand new widget
+var newWidget = document.getElementById("new-widget");
+newWidget.getElementsByClassName("header")[0].onclick = function () {
+    if (newWidget.className == "widget") {
+        newWidget.className = "widget collapsed";
+    } else {
+        newWidget.className = "widget";
+    }
+};
+
+//collapse/expand search widget
+var searchWidget = document.getElementById("google-search");
+searchWidget.getElementsByClassName("header")[0].onclick = function () {
+    if (searchWidget.className == "widget") {
+        searchWidget.className = "widget collapsed";
+    } else {
+        searchWidget.className = "widget";
+    }
+};
+
 // actions for top links
 var links = document.getElementById("top-menu").getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
